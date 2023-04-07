500c622063eb7edcbe12ff01736f1d97d0cb4492
Rounak <rounaksaraf.official@gmail.com>
2023-04-07 17:45:44 +0530

    restructured folder strcuture

--- inotebook/src/components/About.js
+++
@@ -1,9 +0,0 @@
-const About = () => {
-    return (
-        <div class="my-3">
-            This is a cloud notebook app for users where they can login and store their notes.
-        </div>
-    )
-}
-
-export default About
